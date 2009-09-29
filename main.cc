44e4e5ed3fcd44bc269f082a48164f3b9820c3eb
Dustin Sallings <dustin@spy.net>
2009-09-29 14:22:17 -0700

    Use an stl map for a test test.

--- main.cc
+++ main.cc
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <iostream>
+#include <map>
 
 #include "base-test.hh"
 
 class SimpleThing : public kvtest::ThingUnderTest {
 public:
     bool set(std::string &key, std::string &val) {
+        storage[key] = val;
         return true;
     }
 
     std::string get(std::string &key) {
-        return NULL;
+        return storage.find(key)->second;
     }
+
+    bool del(std::string &key) {
+        bool rv = true;
+        if(storage.find(key) == storage.end()) {
+            rv = false;
+        }
+        storage.erase(key);
+        return rv;
+    }
+
+private:
+    std::map<std::string, std::string> storage;
 };
 
 int main(int argc, char **args) {
