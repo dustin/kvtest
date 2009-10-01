8065b1daadb236bd0dec57c66590a84688e0cc81
Dustin Sallings <dustin@spy.net>
2009-09-30 21:52:58 -0700

    Mutex/cond callbacks.

--- tests.cc
+++ tests.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <signal.h>
+#include <pthread.h>
 #include <iostream>
 #include <sstream>
 
@@ -34,15 +35,66 @@ static void setup_alarm(int duration) {
 // ----------------------------------------------------------------------
 //
 
+class LockHolder {
+public:
+    LockHolder(pthread_mutex_t *m) {
+        mutex = m;
+        if(pthread_mutex_lock(mutex) != 0) {
+            throw std::runtime_error("Failed to acquire lock.");
+        }
+    };
+
+    ~LockHolder() {
+        pthread_mutex_unlock(mutex);
+    }
+
+private:
+    pthread_mutex_t *mutex;
+};
+
 template <typename T>
 class RememberingCallback : public Callback<T> {
 public:
+    RememberingCallback() {
+        if(pthread_mutex_init(&mutex, NULL) != 0) {
+            throw std::runtime_error("Failed to initialize mutex.");
+        }
+        if(pthread_cond_init(&cond, NULL) != 0) {
+            throw std::runtime_error("Failed to initialize condition.");
+        }
+        fired = false;
+    };
+
+    ~RememberingCallback() {
+        pthread_mutex_destroy(&mutex);
+        pthread_cond_destroy(&cond);
+    }
+
     void callback(T &value) {
+        LockHolder lh(&mutex);
         val = value;
         fired = true;
-    }
-    T val;
-    bool fired;
+        if(pthread_cond_broadcast(&cond) != 0) {
+            throw std::runtime_error("Failed to broadcast change.");
+        }
+    };
+
+    void waitForValue() {
+        LockHolder lh(&mutex);
+        if (!fired) {
+            if(pthread_cond_wait(&cond, &mutex) != 0) {
+                throw std::runtime_error("Failed to wait for condition.");
+            }
+        }
+        assert(fired);
+    };
+
+    T               val;
+    bool            fired;
+
+private:
+    pthread_mutex_t mutex;
+    pthread_cond_t  cond;
 };
 
 bool TestTest::run(ThingUnderTest *tut) {
@@ -52,26 +104,31 @@ bool TestTest::run(ThingUnderTest *tut) {
     RememberingCallback<bool> cb;
     assertFalse(cb.fired, "Expected callback to not have fired");
     tut->set(testKey, testValue, cb);
+    cb.waitForValue();
     assertTrue(cb.fired, "Expected callback to have fired.");
     assertTrue(cb.val, string("Failed to set value."));
 
-    RememberingCallback<std::string*> getCb;
+    RememberingCallback<GetValue> getCb;
     assertFalse(getCb.fired, "Expected callback to not have fired");
     tut->get(testKey, getCb);
-    assertNotNull(getCb.val);
-    assertEquals(*getCb.val, testValue);
+    cb.waitForValue();
+    assertTrue(getCb.val.success, "Expected success getting initial value");
+    assertEquals(getCb.val.value, testValue);
 
     RememberingCallback<bool> delCb1;
     tut->del(testKey, delCb1);
+    cb.waitForValue();
     assertTrue(delCb1.val,  string("Failed to delete value."));
 
     RememberingCallback<bool> delCb2;
     tut->del(testKey, delCb2);
+    cb.waitForValue();
     assertFalse(delCb2.val, string("Doubly deleted value"));
 
-    RememberingCallback<std::string*> getCb2;
+    RememberingCallback<GetValue> getCb2;
     tut->get(testKey, getCb2);
-    assertNull(getCb2.val);
+    cb.waitForValue();
+    assertFalse(getCb2.val.success, "Expected failure getting final value.");
 }
 
 bool WriteTest::run(ThingUnderTest *tut) {
