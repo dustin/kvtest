54663779f854383dfab398d5d811129bd56dba07
Dustin Sallings <dustin@spy.net>
2009-10-01 21:52:41 -0700

    Added sqlite3 async test

--- Makefile
+++ Makefile
@@ -3,9 +3,11 @@ LDFLAGS=-g
 
 COMMON=base-test.hh locks.hh callbacks.hh suite.hh tests.hh
 OBJS=tests.o suite.o
-PROG_OBJS=example-test.o sqlite3-test.o
-ALL_OBJS=$(OBJS) $(PROG_OBJS)
-ALL_PROGS=example-test sqlite3-test
+PROG_OBJS=example-test.o sqlite3-test.o sqlite3-async-test.o
+SQLITE_OBJS=sqlite-base.o
+SQLITE_COMMON=sqlite-base.hh
+ALL_OBJS=$(OBJS) $(SQLITE_OBJS) $(PROG_OBJS)
+ALL_PROGS=example-test sqlite3-test sqlite3-async-test
 
 .PHONY: clean
 
@@ -14,8 +16,11 @@ all: $(ALL_PROGS)
 example-test: example-test.o $(OBJS) $(COMMON)
 	$(CXX) -o $@ example-test.o $(OBJS) $(LDFLAGS) -lsqlite3
 
-sqlite3-test: sqlite3-test.o sqlite-base.o $(OBJS) $(COMMON)
-	$(CXX) -o $@ sqlite3-test.o sqlite-base.o $(OBJS) $(LDFLAGS) -lsqlite3
+sqlite3-test: sqlite3-test.o $(SQLITE_OBJS) $(OBJS) $(COMMON)
+	$(CXX) -o $@ sqlite3-test.o $(SQLITE_OBJS) $(OBJS) $(LDFLAGS) -lsqlite3
+
+sqlite3-async-test: sqlite3-async-test.o $(SQLITE_OBJS) $(OBJS) $(COMMON)
+	$(CXX) -o $@ sqlite3-async-test.o $(SQLITE_OBJS) $(OBJS) $(LDFLAGS) -lsqlite3
 
 clean:
 	-rm $(ALL_OBJS) $(ALL_PROGS)
@@ -26,4 +31,5 @@ clean:
 $(OBJS): $(COMMON)
 $(PROG_OBJS): $(COMMON)
 
-sqlite3-test.o: sqlite-base.hh
+$(SQLITE_OBJS): $(SQLITE_COMMON)
+
---
+++ sqlite3-async-test.cc
@@ -0,0 +1,225 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <iostream>
+#include <queue>
+
+#include "base-test.hh"
+#include "suite.hh"
+#include "sqlite-base.hh"
+
+using namespace kvtest;
+
+class DBOperation {
+public:
+    virtual bool execute(Sqlite3 *db) {}
+};
+
+class ResetOperation : public DBOperation {
+public:
+
+    ResetOperation(Callback<bool> *c) {
+        cb = c;
+    }
+
+    bool execute(Sqlite3 *db) {
+        db->rollback();
+        db->reset();
+        bool rv = true;
+        cb->callback(rv);
+    }
+
+private:
+    Callback<bool> *cb;
+};
+
+class SetOperation : public DBOperation {
+public:
+
+    SetOperation(std::string &k, std::string &v,
+                 Callback<bool> *c) {
+        key = k;
+        value = v;
+        cb = c;
+    }
+
+    bool execute(Sqlite3 *db) {
+        db->set(key, value, *cb);
+    }
+
+private:
+    std::string     key;
+    std::string     value;
+    Callback<bool> *cb;
+};
+
+class GetOperation : public DBOperation {
+public:
+
+    GetOperation(std::string &k, Callback<GetValue> *c) {
+        key = k;
+        cb = c;
+    }
+
+    bool execute(Sqlite3 *db) {
+        db->get(key, *cb);
+    }
+
+private:
+    std::string                         key;
+    Callback<GetValue> *cb;
+};
+
+class DeleteOperation : public DBOperation {
+public:
+
+    DeleteOperation(std::string &k, Callback<bool> *c) {
+        key = k;
+        cb = c;
+    }
+
+    bool execute(Sqlite3 *db) {
+        db->del(key, *cb);
+    }
+
+private:
+    std::string             key;
+    Callback<bool> *cb;
+};
+
+class InboundQueue {
+public:
+    InboundQueue() {
+        pthread_mutex_init(&mutex, NULL);
+        pthread_cond_init(&cond, NULL);
+    }
+
+    ~InboundQueue() {
+        pthread_cond_destroy(&cond);
+        pthread_mutex_destroy(&mutex);
+    }
+
+    void addOperation(DBOperation *op) {
+        LockHolder lh(&mutex);
+        ops.push(op);
+        if(pthread_cond_signal(&cond) != 0) {
+            throw std::runtime_error("Error signaling change.");
+        }
+    }
+
+    void drainTo(std::queue<DBOperation*> &out) {
+        LockHolder lh(&mutex);
+        if(ops.empty()) {
+            if(pthread_cond_wait(&cond, &mutex) != 0) {
+                throw std::runtime_error("Error waiting for signal.");
+            }
+        }
+        while(!ops.empty()) {
+            out.push(ops.front());
+            ops.pop();
+        }
+    }
+
+private:
+    pthread_mutex_t          mutex;
+    pthread_cond_t           cond;
+    std::queue<DBOperation*> ops;
+};
+
+class QueryExecutor {
+public:
+    QueryExecutor(Sqlite3 *d, InboundQueue *q) {
+        db = d;
+        iq = q;
+        running = true;
+    }
+
+    void run() {
+        while(running) {
+            std::queue<DBOperation*> ops;
+            iq->drainTo(ops);
+            db->begin();
+            int count = 0;
+
+            while(!ops.empty()) {
+                DBOperation *op = ops.front();
+                ops.pop();
+
+                op->execute(db);
+                count++;
+            }
+
+            db->commit();
+            std::cout << "Executed " << count << " operations" << std::endl;
+        }
+    }
+
+private:
+    bool          running;
+    Sqlite3      *db;
+    InboundQueue *iq;
+};
+
+static void* launch_executor_thread(void* arg) {
+    QueryExecutor *executor = (QueryExecutor*) arg;
+    try {
+        executor->run();
+    } catch(...) {
+        std::cerr << "Caught a fatal exception in the thread" << std::endl;
+    }
+}
+
+class QueuedSqlite : public ThingUnderTest {
+public:
+
+    QueuedSqlite(const char *path) {
+        db = new Sqlite3(path);
+        iq = new InboundQueue();
+        executor = new QueryExecutor(db, iq);
+
+        if(pthread_create(&thread, NULL, launch_executor_thread, executor)
+           != 0) {
+            throw std::runtime_error("Error initializing queue thread");
+        }
+    }
+
+    ~QueuedSqlite() {
+        delete db;
+        delete iq;
+        delete executor;
+    }
+
+    void reset() {
+        std::queue<DBOperation*> opq;
+        RememberingCallback<bool> cb;
+        iq->addOperation(new ResetOperation(&cb));
+        cb.waitForValue();
+    }
+
+    void set(std::string &key, std::string &val,
+             Callback<bool> &cb) {
+        iq->addOperation(new SetOperation(key, val, &cb));
+    }
+
+    void get(std::string &key, Callback<GetValue> &cb) {
+        iq->addOperation(new GetOperation(key, &cb));
+    }
+
+    void del(std::string &key, Callback<bool> &cb) {
+        iq->addOperation(new DeleteOperation(key, &cb));
+    }
+
+private:
+    Sqlite3       *db;
+    InboundQueue  *iq;
+    QueryExecutor *executor;
+    pthread_t      thread;
+};
+
+int main(int argc, char **args) {
+    QueuedSqlite *thing = new QueuedSqlite("/tmp/test.db");
+
+    TestSuite suite(thing);
+    return suite.run() ? 0 : 1;
+}
