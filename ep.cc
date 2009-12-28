8de807146839e5586140dab59e20f6b3d1261075
Dustin Sallings <dustin@spy.net>
2009-12-28 15:09:36 -0800

    Async ep writes.

--- ep.cc
+++ ep.cc
@@ -1,20 +1,55 @@
 #include "ep.hh"
+#include "locks.hh"
 
 namespace kvtest {
 
+    static void* launch_flusher_thread(void* arg) {
+        Flusher *flusher = (Flusher*) arg;
+        try {
+            flusher->run();
+        } catch(...) {
+            std::cerr << "Caught a fatal exception in the thread" << std::endl;
+        }
+        return NULL;
+    }
+
     EventuallyPersistentStore::EventuallyPersistentStore(KVStore *t) {
+        pthread_mutex_init(&mutex, NULL);
+        pthread_cond_init(&cond, NULL);
+        towrite = new std::queue<std::string>;
+        flusher = new Flusher(this);
+
+        // Run in a thread...
+        if(pthread_create(&thread, NULL, launch_flusher_thread, flusher)
+           != 0) {
+            throw std::runtime_error("Error initializing queue thread");
+        }
+
         underlying = t;
+        assert(underlying);
     }
 
     EventuallyPersistentStore::~EventuallyPersistentStore() {
+        LockHolder lh(&mutex);
+        flusher->stop();
+        if(pthread_cond_signal(&cond) != 0) {
+            throw std::runtime_error("Error signaling change.");
+        }
+        lh.unlock();
+        pthread_join(thread, NULL);
+        delete flusher;
+        delete towrite;
         for (std::map<std::string, StoredValue*>::iterator it=storage.begin();
              it != storage.end(); it++ ) {
             delete (*it).second;
         }
+        pthread_cond_destroy(&cond);
+        pthread_mutex_destroy(&mutex);
     }
 
     void EventuallyPersistentStore::set(std::string &key, std::string &val,
                                         Callback<bool> &cb) {
+        LockHolder lh(&mutex);
         std::map<std::string, StoredValue*>::iterator it = storage.find(key);
         if (it != storage.end()) {
             delete it->second;
@@ -22,25 +57,103 @@ namespace kvtest {
 
         storage[key] = new StoredValue(val);
         bool rv = true;
+        markDirty(key);
+        lh.unlock();
         cb.callback(rv);
+    }
+
+    void EventuallyPersistentStore::reset() {
+        flush(false);
+        LockHolder lh(&mutex);
+        underlying->reset();
+        delete towrite;
+        towrite = new std::queue<std::string>;
+        storage.clear();
     }
 
     void EventuallyPersistentStore::get(std::string &key,
                                         Callback<kvtest::GetValue> &cb) {
+        LockHolder lh(&mutex);
         std::map<std::string, StoredValue*>::iterator it = storage.find(key);
         bool success = it != storage.end();
         kvtest::GetValue rv(success ? it->second->getValue() : std::string(":("),
                             success);
+        lh.unlock();
         cb.callback(rv);
     }
 
     void EventuallyPersistentStore::del(std::string &key, Callback<bool> &cb) {
         bool rv = true;
+        LockHolder lh(&mutex);
         if(storage.find(key) == storage.end()) {
             rv = false;
+        } else {
+            markDirty(key);
+            storage.erase(key);
         }
-        storage.erase(key);
+        lh.unlock();
         cb.callback(rv);
+    }
+
+    void EventuallyPersistentStore::markDirty(std::string &key) {
+        std::map<std::string, StoredValue*>::iterator it = storage.find(key);
+        if (it != storage.end()) {
+            it->second->markDirty();
+        }
+        towrite->push(key);
+        if(pthread_cond_signal(&cond) != 0) {
+            throw std::runtime_error("Error signaling change.");
+        }
+    }
+
+    void EventuallyPersistentStore::flush(bool shouldWait) {
+        LockHolder lh(&mutex);
+
+        if (towrite->empty()) {
+            if (shouldWait) {
+                if(pthread_cond_wait(&cond, &mutex) != 0) {
+                    throw std::runtime_error("Error waiting for signal.");
+                }
+            }
+        } else {
+            std::queue<std::string> *q = towrite;
+            towrite = new std::queue<std::string>;
+            lh.unlock();
+
+            RememberingCallback<bool> cb;
+            assert(underlying);
+            underlying->begin();
+            while (!q->empty()) {
+                std::string key = q->front();
+                flushOne(key, cb);
+                q->pop();
+            }
+            underlying->commit();
+        }
+    }
+
+    void EventuallyPersistentStore::flushOne(std::string &key,
+                                             Callback<bool> &cb) {
+        LockHolder lh(&mutex);
+        std::map<std::string, StoredValue*>::iterator it = storage.find(key);
+        bool found = it != storage.end();
+        bool isDirty = (found && it->second->isDirty());
+        std::string val;
+        if (found && it->second->isDirty()) {
+            it->second->markClean();
+            val = it->second->getValue();
+        }
+        lh.unlock();
+
+        if (isDirty) {
+            // should set
+            underlying->set(key, val, cb);
+        } else if (!found) {
+            // Should delete
+            underlying->del(key, cb);
+        } else {
+            std::cout << "skipping write of ``" << key << "''" << std::endl;
+        }
     }
 
 }
--- ep.hh
+++ ep.hh
@@ -1,6 +1,7 @@
 #ifndef EP_HH
 #define EP_HH 1
 
+#include <pthread.h>
 #include <assert.h>
 #include <stdbool.h>
 #include <stdexcept>
@@ -24,6 +25,9 @@ namespace kvtest {
         void markClean() {
             dirty = false;
         }
+        bool isDirty() {
+            return dirty;
+        }
         std::string getValue() {
             return value;
         }
@@ -31,6 +35,9 @@ namespace kvtest {
         bool dirty;
         std::string value;
     };
+
+    // Forward declaration
+    class Flusher;
 
     class EventuallyPersistentStore : public KVStore {
     public:
@@ -46,11 +53,55 @@ namespace kvtest {
 
         void del(std::string &key, Callback<bool> &cb);
 
+        void reset();
+
     private:
-        KVStore *underlying;
-        std::map<std::string, StoredValue*> storage;
+
+        void markDirty(std::string &key);
+        void flush(bool shouldWait);
+        void flushOne(std::string &key, Callback<bool> &cb);
+
+        friend class Flusher;
+
+        KVStore                             *underlying;
+        Flusher                             *flusher;
+        std::map<std::string, StoredValue*>  storage;
+        pthread_mutex_t                      mutex;
+        pthread_cond_t                       cond;
+        std::queue<std::string>             *towrite;
+        pthread_t                            thread;
         DISALLOW_COPY_AND_ASSIGN(EventuallyPersistentStore);
     };
+
+    class Flusher {
+    public:
+        Flusher(EventuallyPersistentStore *st) {
+            store = st;
+            running = true;
+        }
+        ~Flusher() {
+            stop();
+        }
+        void stop() {
+            running = false;
+        }
+        void run() {
+            try {
+                while(running) {
+                    store->flush(true);
+                }
+                std::cout << "Shutting down flusher." << std::endl;
+            } catch(std::runtime_error &e) {
+                std::cerr << "Exception in executor loop: "
+                          << e.what() << std::endl;
+                assert(false);
+            }
+        }
+    private:
+        EventuallyPersistentStore *store;
+        volatile bool running;
+    };
+
 }
 
 #endif /* EP_HH */
